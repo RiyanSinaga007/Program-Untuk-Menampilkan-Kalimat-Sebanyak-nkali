870ce39fb810b663fa10ab8466a62f1ecf5a183d
RiyanSinaga007 <45660151+RiyanSinaga007@users.noreply.github.com>
2018-12-22 23:43:40 +0700

    Add files via upload

---
+++ Contoh Pengulangan Sebanyak n kali.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(){
+	int i,n;
+	for(i=1; i<=30; i++){
+		for(n = 1;n<=0; n++){
+			printf("%i",i);
+		}
+		printf("\n Algoritma Dan Pemrograman");
+	}
+	return 0;
+}
+
+
